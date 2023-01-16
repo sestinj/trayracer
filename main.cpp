5940a9e48bf3425a97c092cfe40c1f3ca11a1e2a
Nate Sesti <sestinj@gmail.com>
2023-01-16 02:39:09 -0500

    TBB and omplib

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include "camera.h"
 #include "material.h"
 
+// #include "/opt/homebrew/Cellar/tbb/2021.8.0/include/tbb/parallel_for.h"
+#include "/opt/homebrew/Cellar/libomp/15.0.7/include/omp.h"
+
 #include <iostream>
 
 color ray_color(const ray& r, const hittable& world, int depth) {
@@ -101,6 +104,14 @@ int main() {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             color pixel_color(0, 0, 0);
+
+            // tbb::parallel_for(0, samples_per_pixel, [&](int s) {
+            //     auto u = (i + random_double()) / (image_width-1);
+            //     auto v = (j + random_double()) / (image_height-1);
+            //     ray r = cam.get_ray(u, v);
+            //     pixel_color += ray_color(r, world, max_depth);
+            // });
+            #pragma omp parallel for
             for (int s = 0; s < samples_per_pixel; ++s) {
                 auto u = (i + random_double()) / (image_width-1);
                 auto v = (j + random_double()) / (image_height-1);
